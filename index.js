b1ce362c0c9bd11a184c6625e0c936227b2af0a8
Igor Veyner <igorveyner95@gmail.com>
2020-03-10 15:21:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function total() {
 function removeFromCart(item) {
   for(let i=0;i<cart.length;i++){
     if(cart[i].itemName === item){
-      cart.splice()
+      cart.splice(i,)
     }
   }
 }
